e4127c7d42ba8c5d4c2612b758135e14a2d7a2f6
helarye <helarye@gmail.com>
2024-04-29 14:11:11 +0300

    check palindrom runc

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int[] arr2check = {1, 2, 3, 3,2,1};
+int[] arr2Check = {1, 2, 3,4, 3,2,1};
 Console.WriteLine("Hello, World!");
-PrintArr1(arr2check);
+PrintArr1(arr2Check);
+bool isPalindrom = true;
+int maxArrIndx = arr2Check.Length - 1;
+int halfL = arr2Check.Length / 2;
+for (int i = 0; i <halfL ; i++)
+{
+    Console.WriteLine(arr2Check[i] + " " + arr2Check[maxArrIndx - i]);
+    if (arr2Check[i] != arr2Check[maxArrIndx-i])
+    { isPalindrom = false; break; }
+}
+PrintArr1(arr2Check);
+if (isPalindrom)
+{
+    Console.WriteLine(" Is Palindrom");
+}
+else Console.WriteLine(" Is not Palindrom");
+
 static void PrintArr(int[] arr)
 {
     Console.Write("[");
